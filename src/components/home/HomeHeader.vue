7c5ad409f1c0f2a53da33ab35bad6239ac5c0387
unknown <tzhang996527@hotmail.com>
2019-06-01 21:15:28 +0800

    main page

--- src/components/home/HomeHeader.vue
+++ src/components/home/HomeHeader.vue
@@ -7,8 +7,7 @@
       @select="handleSelect"
       background-color="#545c64"
       text-color="#fff"
-      active-text-color="#ffd04b"
-    >
+      active-text-color="#ffd04b">
       <el-menu-item index="1">处理中心</el-menu-item>
       <el-submenu index="2">
         <template slot="title">我的工作台</template>
--- src/router/index.js
+++ src/router/index.js
@@ -3,27 +3,60 @@ import Router from 'vue-router'
 import BlogLogin from '@/components/BlogLogin.vue'
 import HomeIndex from '@/components/home/HomeIndex.vue'
 
+import Home from '@/components/Home'
+// import Login from '@/components/Login'
+import Page1 from '@/components/page1'
+import Page2 from '@/components/page2'
+import Page3 from '@/components/page3'
+
 Vue.use(Router)
 
+// 嵌套路由
+// https://router.vuejs.org/zh/guide/essentials/nested-routes.html
 export default new Router({
   routes: [
     {
       path: '/',
-      redirect: '/login'
-    },
-    {
-      path: '/index',
-      name: 'HomeIndex',
-      component: HomeIndex
-    },
-    {
-      path: '/manage',
-      redirect: '/login'
-    },
-    {
-      path: '/login',
-      name: 'BlogLogin',
-      component: BlogLogin
+      name: 'home',
+      component: Home,
+　　　 redirect:"Page1",//重定向，第一次进入就进入Page1，不添加的话第一次进入右侧是空白
+      children:[
+        {
+          path: '/Page1',
+          name: 'Page1',
+          component: Page1
+        },
+        {
+          path: '/Page2',
+          name: 'Page2',
+          component: Page2
+        },
+        {
+          path: '/Page3',
+          name: 'Page3',
+          component: Page3
+        }
+      ]
     }
   ]
+  // routes: [
+  //   {
+  //     path: '/',
+  //     redirect: '/login'
+  //   },
+  //   {
+  //     path: '/index',
+  //     name: 'HomeIndex',
+  //     component: HomeIndex
+  //   },
+  //   {
+  //     path: '/manage',
+  //     redirect: '/login'
+  //   },
+  //   {
+  //     path: '/login',
+  //     name: 'BlogLogin',
+  //     component: BlogLogin
+  //   }
+  // ]
 })
