0311848fe89f949b11b4c57bc482fb9157cc9dd8
zhang <tzhang996527@hotmail.com>
2019-06-19 17:32:20 +0800

    tour info

--- src/components/Tour/TourList.vue
+++ src/components/Tour/TourList.vue
@@ -3,51 +3,30 @@
     <el-row>
       <el-col :span="12">
         <div class="grid-content bg-purple">
-          <el-form ref="form" :model="form" label-width="80px">
+          <el-form ref="form" :model="header" label-width="80px">
             <el-form-item label="行程编号">
-              <el-input v-model="form.tourid"></el-input>
+              <el-input v-model="header.tourid"></el-input>
             </el-form-item>
             <el-form-item label="发车地点">
-              <el-select v-model="form.region" placeholder="请选择活动区域">
-                <el-option label="区域一" value="shanghai"></el-option>
-                <el-option label="区域二" value="beijing"></el-option>
-              </el-select>
+              <el-select v-model="header.sourceLoc.address"></el-select>
             </el-form-item>
             <el-form-item label="出发时间">
               <el-col :span="11">
-                <el-date-picker
-                  type="date"
-                  placeholder="选择日期"
-                  v-model="form.date1"
-                  style="width: 100%;"
-                ></el-date-picker>
+                <el-date-picker type="date" v-model="header.planDepart" style="width: 100%;"></el-date-picker>
               </el-col>
-              <el-col class="line" :span="2">-</el-col>
-              <el-col :span="11">
-                <el-time-picker placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
-              </el-col>
+            </el-form-item>
+            <el-form-item label="目的地址">
+              <el-select v-model="header.destloc.address"></el-select>
             </el-form-item>
             <el-form-item label="预计达到">
               <el-col :span="11">
-                <el-date-picker
-                  type="date"
-                  placeholder="选择日期"
-                  v-model="form.date1"
-                  style="width: 100%;"
-                ></el-date-picker>
+                <el-date-picker type="date" v-model="header.planArr" style="width: 100%;"></el-date-picker>
               </el-col>
-              <el-col class="line" :span="2">-</el-col>
-              <el-col :span="11">
-                <el-time-picker placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
-              </el-col>
-            </el-form-item>
-            <el-form-item label="即时配送">
-              <el-switch v-model="form.delivery"></el-switch>
             </el-form-item>
             <!-- <el-form-item>
               <el-button type="primary" @click="onSubmit">立即创建</el-button>
               <el-button>取消</el-button>
-            </el-form-item> -->
+            </el-form-item>-->
           </el-form>
         </div>
       </el-col>
@@ -74,23 +53,24 @@
 export default {
   data() {
     return {
-      activeName: 'second',
-      form: {
-        name: "",
-        region: "",
-        date1: "",
-        date2: "",
-        delivery: false,
-        type: [],
-        resource: "",
-        desc: "",
-        tourid:""
+      activeName: "second",
+      header: {
+        tourid: "",
+        sourceLoc: {
+          address: ""
+        },
+        destloc: {
+          address: ""
+        },
+        planDepart: "",
+        planArr: ""
       }
     };
   },
-  created(){
-    debugger
+  created() {
+    debugger;
     this.getParams();
+    this.getTour();
   },
   methods: {
     onSubmit() {
@@ -98,16 +78,42 @@ export default {
     },
     handleClick(tab, event) {
       // console.log(this.$parent.$data.formInline.tourid);
-        console.log(tab, event);
+      console.log(tab, event);
     },
-    getParams:function(){
-    // 取到路由带过来的参数
-    debugger
-    var routerParams = this.$route.params.id;
-    // 将数据放在当前组件的数据内
-    console.log("传来的参数=="+routerParams);
-    this.form.tourid = routerParams;
-  }
+    getParams: function() {
+      // 取到路由带过来的参数
+      debugger;
+      var routerParams = this.$route.params.id;
+      // 将数据放在当前组件的数据内
+      console.log("传来的参数==" + routerParams);
+      this.header.tourid = routerParams;
+    },
+    getTour() {
+      this.$axios
+        .get("/api/v1/tourDetail", {
+          params: {
+            tourid: this.header.tourid
+          }
+        })
+        .then(response => {
+          this.header = response.data[0];
+          debugger;
+          // this.header = {
+          //   tourid: "",
+          //   sourceLoc: {
+          //     address: ""
+          //   },
+          //   destloc: {
+          //     address: ""
+          //   },
+          //   planDepart: "",
+          //   planArr: ""
+          // };
+        })
+        .catch(function(error) {
+          alert(error);
+        });
+    }
   }
 };
 </script>
